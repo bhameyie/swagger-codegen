0d3d911c33cf130de0a3cb6dbb139c97d4c1b9f4
Boguste Hameyie <boguste.hameyie@sungardas.com>
2016-08-30 10:56:41 -0400

    added event to the list of reserved key word

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/SwiftCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/SwiftCodegen.java
@@ -108,6 +108,7 @@ public class SwiftCodegen extends DefaultCodegen implements CodegenConfig {
                     "true", "lazy", "operator", "in", "COLUMN", "left", "private", "return", "FILE", "mutating", "protocol",
                     "switch", "FUNCTION", "none", "public", "where", "LINE", "nonmutating", "static", "while", "optional",
                     "struct", "override", "subscript", "postfix", "typealias", "precedence", "var", "prefix", "Protocol",
+                    "event", "Event",
                     "required", "right", "set", "Type", "unowned", "weak")
                 );
 
